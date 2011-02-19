b82085d0dff24c9b13898a42ba2c05a22c15d0ad
llasry@cisco.com <llasry@cisco.com@1f3fe170-bcf4-748c-f3f9-3c66050806bb>
2011-02-19 17:31:16 +0000

    removed simplenavigateview

--- trunk/CaptureAnalyzer/src/capanalyzer/Perspective.java
+++ trunk/CaptureAnalyzer/src/capanalyzer/Perspective.java
@@ -4,21 +4,19 @@ import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
-public class Perspective implements IPerspectiveFactory {
+public class Perspective implements IPerspectiveFactory
+{
 
 	private final String NAVIGATOR_ID = "capanalyzer.navigatorView";
-	private final String SIMPLENAVIGATOR_ID = "capanalyzer.simplenavigateview";
 
-	public void createInitialLayout(IPageLayout layout) {
+	public void createInitialLayout(IPageLayout layout)
+	{
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(false);
 
-		IFolderLayout navFolder = layout.createFolder("navigate",
-				IPageLayout.LEFT, 0.25f, editorArea);
+		IFolderLayout navFolder = layout.createFolder("navigate", IPageLayout.LEFT, 0.25f, editorArea);
 		navFolder.addView(NAVIGATOR_ID);
-		navFolder.addView(SIMPLENAVIGATOR_ID);
-		IFolderLayout folder = layout.createFolder("messages", IPageLayout.TOP,
-				0.5f, editorArea);
+		IFolderLayout folder = layout.createFolder("messages", IPageLayout.TOP, 0.5f, editorArea);
 		folder.addPlaceholder(MessageView.ID + ":*");
 		folder.addView(MessageView.ID);
 		layout.addView(MessageTableView.ID, IPageLayout.TOP, 0.45f, "messages");
