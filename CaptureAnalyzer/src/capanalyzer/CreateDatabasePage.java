293795af0c5f51337c2937248865bc63616845a8
llasry <llasry@cisco.com>
2011-02-26 09:16:03 +0000

    updated for database parameters

--- CaptureAnalyzer/src/capanalyzer/CreateDatabasePage.java
+++ CaptureAnalyzer/src/capanalyzer/CreateDatabasePage.java
@@ -28,41 +28,46 @@ import org.eclipse.swt.widgets.Text;
 
 import capanalyzer.model.Database;
 
-
-public class CreateDatabasePage extends WizardPage {
+public class CreateDatabasePage extends WizardPage
+{
 
 	private static HashMap<Control, ControlDecoration> decoratorMap = new HashMap<Control, ControlDecoration>();
 
-	public static class HostnameValidator implements IValidator {
+	public static class HostnameValidator implements IValidator
+	{
 
-		public IStatus validate(Object value) {
+		public IStatus validate(Object value)
+		{
 			String string = (String) value;
-			if (string == null || string.trim().length() == 0) {
-				return ValidationStatus
-						.error("Please enter a value for Hostname.");
+			if (string == null || string.trim().length() == 0)
+			{
+				return ValidationStatus.error("Please enter a value for Hostname.");
 			}
-			if (!string.matches("^[-\\w.]+$")) {
-				return ValidationStatus
-						.error("Hostname must be of the form xxx.xxx.");
+			if (!string.matches("^[-\\w.]+$"))
+			{
+				return ValidationStatus.error("Hostname must be of the form xxx.xxx.");
 			}
 			return ValidationStatus.ok();
 		}
 
 	}
 
-	public static class NotEmptyValidator implements IValidator {
+	public static class NotEmptyValidator implements IValidator
+	{
 
 		private final String fieldname;
 
-		public NotEmptyValidator(String fieldname) {
+		public NotEmptyValidator(String fieldname)
+		{
 			this.fieldname = fieldname;
 		}
 
-		public IStatus validate(Object value) {
+		public IStatus validate(Object value)
+		{
 			String string = (String) value;
-			if (string == null || string.trim().length() == 0) {
-				return ValidationStatus.error("Please enter a value for "
-						+ fieldname + ".");
+			if (string == null || string.trim().length() == 0)
+			{
+				return ValidationStatus.error("Please enter a value for " + fieldname + ".");
 			}
 			return ValidationStatus.ok();
 		}
@@ -71,14 +76,16 @@ public class CreateDatabasePage extends WizardPage {
 
 	private final Database database;
 
-	public CreateDatabasePage(Database database) {
+	public CreateDatabasePage(Database database)
+	{
 		super("wizardPage");
 		this.database = database;
 		setTitle("Create Database Connection");
 		setDescription("Please enter information to create a database connection.");
 	}
 
-	public void createControl(Composite parent) {
+	public void createControl(Composite parent)
+	{
 		Composite container = new Composite(parent, SWT.NULL);
 		setControl(container);
 
@@ -101,77 +108,74 @@ public class CreateDatabasePage extends WizardPage {
 		passwordText.setEchoChar('*');
 		createControlDecoration(passwordText);
 
-		Label portLabel = new Label(container, SWT.NONE);
-		portLabel.setText("Port");
+		Label schemaLabel = new Label(container, SWT.NONE);
+		schemaLabel.setText("Schema");
 
-		Text portText = new Text(container, SWT.BORDER);
-		createControlDecoration(portText);
-		
+		Text schemaText = new Text(container, SWT.BORDER);
+		createControlDecoration(schemaText);
+
 		// The additional spacing (default is 5,5) is for the decorations
-		GridLayoutFactory.swtDefaults().numColumns(2).spacing(10, 5)
-				.generateLayout(container);
+		GridLayoutFactory.swtDefaults().numColumns(2).spacing(10, 5).generateLayout(container);
 
 		final DataBindingContext dbc = new DataBindingContext();
 
 		bind(dbc, hostnameText, database, "hostname", new HostnameValidator());
-		bind(dbc, usernameText, database, "username", new NotEmptyValidator(
-				"username"));
-		bind(dbc, passwordText, database, "password", new NotEmptyValidator(
-				"password"));
-		bind(dbc, portText, database, "port", null);
+		bind(dbc, usernameText, database, "username", new NotEmptyValidator("username"));
+		bind(dbc, passwordText, database, "password", new NotEmptyValidator("password"));
+		bind(dbc, schemaText, database, "schema", null);
 
-		final AggregateValidationStatus aggregateValidationStatus = new AggregateValidationStatus(
-				dbc.getValidationStatusProviders(),
-				AggregateValidationStatus.MAX_SEVERITY);
+		final AggregateValidationStatus aggregateValidationStatus = new AggregateValidationStatus(dbc.getValidationStatusProviders(), AggregateValidationStatus.MAX_SEVERITY);
 
 		aggregateValidationStatus.addValueChangeListener(new IValueChangeListener() {
-					public void handleValueChange(ValueChangeEvent event) {
-						// the invocation of the getValue method is necessary
-						// the further changes will be fired
-						aggregateValidationStatus.getValue();
-						for (Object o : dbc.getBindings()) {
-							Binding binding = (Binding) o;
-							IStatus status = (IStatus) binding.getValidationStatus().getValue();
-							Control control = null;
-							if (binding.getTarget() instanceof ISWTObservable) {
-								ISWTObservable swtObservable = (ISWTObservable) binding.getTarget();
-								control = (Control) swtObservable.getWidget();
-							}
-							ControlDecoration decoration = decoratorMap.get(control);
-							if (decoration != null) {
-								if (status.isOK()) {
-									decoration.hide();
-								} else {
-									decoration.setDescriptionText(status.getMessage());
-									decoration.show();
-								}
-							}
+			public void handleValueChange(ValueChangeEvent event)
+			{
+				// the invocation of the getValue method is necessary the further changes will be fired
+				aggregateValidationStatus.getValue();
+				for (Object o : dbc.getBindings())
+				{
+					Binding binding = (Binding) o;
+					IStatus status = (IStatus) binding.getValidationStatus().getValue();
+					Control control = null;
+					if (binding.getTarget() instanceof ISWTObservable)
+					{
+						ISWTObservable swtObservable = (ISWTObservable) binding.getTarget();
+						control = (Control) swtObservable.getWidget();
+					}
+					ControlDecoration decoration = decoratorMap.get(control);
+					if (decoration != null)
+					{
+						if (status.isOK())
+						{
+							decoration.hide();
+						} else
+						{
+							decoration.setDescriptionText(status.getMessage());
+							decoration.show();
 						}
 					}
-				});
+				}
+			}
+		});
 
 		// create this after binding the observables to avoid displaying an
 		// error when the wizard page is shown:
 		WizardPageSupport.create(this, dbc);
 	}
 
-	private void bind(DataBindingContext dbc, Text textWidget, Object bean,
-			String property, IValidator validator) {
+	private void bind(DataBindingContext dbc, Text textWidget, Object bean, String property, IValidator validator)
+	{
 		UpdateValueStrategy targetToModel = null;
-		if (validator != null) {
-			targetToModel = new UpdateValueStrategy()
-					.setAfterConvertValidator(validator);
+		if (validator != null)
+		{
+			targetToModel = new UpdateValueStrategy().setAfterConvertValidator(validator);
 		}
-		dbc.bindValue(SWTObservables.observeText(textWidget, SWT.Modify),
-				BeansObservables.observeValue(bean, property), targetToModel,
-				null);
+		dbc.bindValue(SWTObservables.observeText(textWidget, SWT.Modify), BeansObservables.observeValue(bean, property), targetToModel, null);
 	}
 
-	private void createControlDecoration(Control control) {
-		ControlDecoration controlDecoration = new ControlDecoration(control,
-				SWT.LEFT | SWT.TOP);
-		FieldDecoration fieldDecoration = FieldDecorationRegistry.getDefault()
-				.getFieldDecoration(FieldDecorationRegistry.DEC_ERROR);
+	private void createControlDecoration(Control control)
+	{
+		ControlDecoration controlDecoration = new ControlDecoration(control, SWT.LEFT | SWT.TOP);
+		FieldDecoration fieldDecoration = FieldDecorationRegistry.getDefault().getFieldDecoration(FieldDecorationRegistry.DEC_ERROR);
 		controlDecoration.setImage(fieldDecoration.getImage());
 		controlDecoration.hide();
 		decoratorMap.put(control, controlDecoration);
