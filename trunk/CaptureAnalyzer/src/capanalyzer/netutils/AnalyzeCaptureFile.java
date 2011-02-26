af1fec67c93b8f1d974111823947895b13d792ab
llasry@cisco.com <llasry@cisco.com@1f3fe170-bcf4-748c-f3f9-3c66050806bb>
2011-02-26 09:18:18 +0000

    Refactored

--- trunk/CaptureAnalyzer/src/capanalyzer/netutils/AnalyzeCaptureFile.java
+++ trunk/CaptureAnalyzer/src/capanalyzer/netutils/AnalyzeCaptureFile.java
@@ -9,6 +9,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import capanalyzer.GlobalConfig;
 import capanalyzer.database.ConnectionPool;
 import capanalyzer.netutils.build.FiveTuple;
 import capanalyzer.netutils.build.IPPacket;
@@ -18,8 +19,7 @@ import capanalyzer.netutils.files.CaptureFileReader;
 
 public class AnalyzeCaptureFile
 {
-	//static String erfFile = "e:\\capture_012_15_06_2009.erf";
-	static String erfFile = "d:\\capture_012_15_06_2009.erf";
+	static String erfFile = "e:\\capture_012_15_06_2009.erf";
 
 	/** JDBC driver name */
 	private static String driverName;
@@ -33,217 +33,182 @@ public class AnalyzeCaptureFile
 	/** JDBC connection password */
 	private static String password;
 
-	@SuppressWarnings("serial")
 	public static void main(String[] args) throws IOException, NetUtilsException
 	{
-		int[] numOfDbPatitions = new int[]{1, 10, 20, 40};
-		int[] sizeOfBuffer = new int[]{8, 16, 32, 64};
-		int[] numOfMaps = new int[]{2, 4, 8, 16};
+		final long agingTime = GlobalConfig.CaptureFileReadParams.getAgingTime() * 1000000;
+		final int numOfIpPacketsBetweenChecks = 6000000;
 		
-		for (int k = 0; k < numOfDbPatitions.length; k++)
-		{
-			for (int l = 0; l < sizeOfBuffer.length; l++)
-			{
-				for (int m = 0; m < numOfMaps.length; m++)
-				{
-			
-					Statisics.setNumOfDbPartitions(numOfDbPatitions[k]);
-					Statisics.setSizeOfBuffer(sizeOfBuffer[l]);
-					Statisics.setNumOfMaps(numOfMaps[m]);
-					
-					boolean takeReadAndStoreInMapTime = true;
-					Statisics.initStatsArrays();
-					
-					long agingTime = 120 * 1000000;
-					List<IPacketAnalyzer> packetAnalyzers = new ArrayList<IPacketAnalyzer>();
-					packetAnalyzers.add(new BaseAnalyzer());
-			
-					CaptureFileReader frd = CaptureFileFactory.createCaptureFileReader(erfFile);
-					CaptureFileBlock nextblock = null;
-					long firstPacketTime = 0, currentPacketTime = 0;
-					long counter = 0;
-			
-					FlowsDataStructure flowsDataStructure = FlowsDataStructure.getInstance();
-					FlowsDataStructureForDB flowsDataStructureForDb = FlowsDataStructureForDB.getInstance();
-					List<FiveTuple> listOfFlowsThatShouldAgeFiveTuples;
-			
-					setMySQLConnectInfo();
-					// SQLExecutor sqlExec2 = new SQLExecutor(getConnectionPool());
-					Connection con = getConnectionPool().getConnection();
-					
-					try
-					{
-						con.createStatement().execute("DROP TABLE IF EXISTS `capture_analyzer`.`all_flows`");
+		List<IPacketAnalyzer> packetAnalyzers = new ArrayList<IPacketAnalyzer>();
+		packetAnalyzers.add(new BaseAnalyzer());
 
-						con.createStatement()
-								.execute(
-										"CREATE TABLE  `capture_analyzer`.`all_flows` " +
-										"(`flow_id` int(10) unsigned NOT NULL DEFAULT '0'," +
-										"`source_ip` bigint(20) unsigned NOT NULL DEFAULT '0'," +
-										"`source_port` int(10) unsigned NOT NULL DEFAULT '0'," +
-										"`destination_ip` bigint(20) unsigned NOT NULL DEFAULT '0'," +
-										"`destination_port` int(10) unsigned NOT NULL DEFAULT '0'," +
-										"`flow_type` int(10) unsigned NOT NULL DEFAULT '0'," +
-										"`start_time` bigint(20) unsigned NOT NULL DEFAULT '0'," +
-										"`duration` bigint(20) unsigned NOT NULL DEFAULT '0'," +
-										"`number_of_packets` bigint(20) unsigned NOT NULL DEFAULT '0'," +
-										"`size` int(10) unsigned NOT NULL DEFAULT '0'," +
-										"`min_packet_size` int(10) unsigned NOT NULL DEFAULT '0'," +
-										"`average_packet_size` int(10) unsigned NOT NULL DEFAULT '0'," +
-										"`max_packet_size` int(10) unsigned NOT NULL DEFAULT '0'," +
-										"`min_ipg` bigint(20) unsigned NOT NULL DEFAULT '0'," +
-										"`average_ipg` bigint(20) unsigned NOT NULL DEFAULT '0'," +
-										"`max_ipg` bigint(20) unsigned NOT NULL DEFAULT '0'," +
-										"PRIMARY KEY (`flow_id`) ENGINE=InnoDB DEFAULT CHARSET=latin1 " +
-										"PARTITION BY HASH(flow_id) " +
-										"PARTITIONS " + Statisics.getNumOfDbPartitions() + "; ");
-					} catch (SQLException e1)
+		CaptureFileReader frd = CaptureFileFactory.createCaptureFileReader(erfFile);
+		CaptureFileBlock nextblock = null;
+		long counter = 0;
+
+		setMySQLConnectInfo();
+		Connection con = getConnectionPool().getConnection();
+		createDbTable(con, true);
+
+		try
+		{
+			long startTime = System.currentTimeMillis();
+			while ((nextblock = frd.readNextBlock()) != null)
+			{
+				if (IPPacket.statIsIpPacket(nextblock.getMyData()))
+				{
+					for (IPacketAnalyzer packetAnalyzer : packetAnalyzers)
 					{
-						e1.printStackTrace();
+						packetAnalyzer.processPacket(nextblock);
 					}
-			
-					try
+
+					counter++;
+					if (counter % numOfIpPacketsBetweenChecks == 0)
 					{
-						long beforeReadAndStoreInMapTime = 0;
-						long startTime = System.currentTimeMillis();
-						while ((nextblock = frd.readNextBlock()) != null)
-						{
-							if(takeReadAndStoreInMapTime == true)
-							{
-								takeReadAndStoreInMapTime = false;
-								beforeReadAndStoreInMapTime = System.currentTimeMillis();
-							}
-								
-								
-							if (IPPacket.statIsIpPacket(nextblock.getMyData()))
-							{
-								for (IPacketAnalyzer packetAnalyzer : packetAnalyzers)
-								{
-									packetAnalyzer.processPacket(nextblock);
-								}
-			
-								if (counter == 0)
-									firstPacketTime = nextblock.getMyPktHdr().getTime();
-			
-								counter++;
-								if (counter % 1000000 == 0)
-								{
-									long afterReadAndStoreInMapTime = System.currentTimeMillis();
-									Statisics.addStatToReadAndStoreInMap(afterReadAndStoreInMapTime-beforeReadAndStoreInMapTime);
-									System.out.println("ReadAndStoreInMapTime= " + (afterReadAndStoreInMapTime-beforeReadAndStoreInMapTime));
-									takeReadAndStoreInMapTime = true;
-									
-									System.out.println("Percentage Done: " + frd.getBytesRead() / (float) frd.getCapFileSizeInBytes());
-			
-									currentPacketTime = nextblock.getMyPktHdr().getTime();
-									listOfFlowsThatShouldAgeFiveTuples = flowsDataStructure.getAllFlowsThatShouldAgeFiveTuples(currentPacketTime, agingTime);
-			
-									System.out.println("Real packet time from start: " + (long)(((currentPacketTime - firstPacketTime) / (long)(1000000 * 60 * 60)) % 24L) + ":" + (long)((currentPacketTime - firstPacketTime) / (long)(1000000 * 60) % 60L) + ":" + (long)(((currentPacketTime - firstPacketTime) / 1000000L) % 60L));
-			
-									for (FiveTuple fiveTuple : listOfFlowsThatShouldAgeFiveTuples)
-									{
-										for (IPacketAnalyzer packetAnalyzer : packetAnalyzers)
-										{
-											packetAnalyzer.finalizeFlow(fiveTuple);
-										}
-									}
-			
-									System.out.print("Nmber of lines in struct = ");
-									long total = 0;
-									long[] mapSizes = flowsDataStructure.getSizesOfAllMaps();
-									for (int i = 0; i < mapSizes.length; i++)
-									{
-										System.out.print(mapSizes[i] + ", ");
-										total += mapSizes[i];
-									}
-									System.out.print("Total = " + total);
-									System.out.println("\n");
-			
-									List<String> allResultNames = new ArrayList<String>() {};
-									PreparedStatement pstmt = null;
-									if (listOfFlowsThatShouldAgeFiveTuples.size() > 0)
-									{
-										String resultNames = "";
-										String resultValues = "";
-			
-										Map<String, Integer> resultIntegerMap = flowsDataStructureForDb.getFlowInfoForDbStruct(listOfFlowsThatShouldAgeFiveTuples.get(0)).getIntegerMap();
-										Map<String, Long> resultLongMap = flowsDataStructureForDb.getFlowInfoForDbStruct(listOfFlowsThatShouldAgeFiveTuples.get(0)).getLongMap();
-										Set<String> allIntResults = resultIntegerMap.keySet();
-										Set<String> allLongResults = resultLongMap.keySet();
-										
-										for (String intKey : allIntResults)
-											allResultNames.add(intKey);
-										for (String longKey : allLongResults)
-											allResultNames.add(longKey);
-			
-										for (String resultName : allResultNames)
-										{
-											resultNames += resultName + ",";
-											resultValues += "?,";
-										}
-										resultNames = resultNames.substring(0, resultNames.length() - 1);
-										resultValues = resultValues.substring(0, resultValues.length() - 1);
-										
-										
-										long beforeInsertTime = System.currentTimeMillis();
-										pstmt = con.prepareStatement("INSERT INTO all_flows(" + resultNames + ") VALUES (" + resultValues + ")");
-										con.setAutoCommit(false);
-										con.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-								
-										for (FiveTuple fiveTuple : listOfFlowsThatShouldAgeFiveTuples)
-										{
-											resultIntegerMap = flowsDataStructureForDb.getFlowInfoForDbStruct(fiveTuple).getIntegerMap();
-											resultLongMap = flowsDataStructureForDb.getFlowInfoForDbStruct(fiveTuple).getLongMap();
-			
-											for (int j = 0; j < allResultNames.size(); j++)
-											{
-												if (resultIntegerMap.containsKey(allResultNames.get(j)))
-													pstmt.setInt(j + 1, resultIntegerMap.get(allResultNames.get(j)));
-												else if (resultLongMap.containsKey(allResultNames.get(j)))
-													pstmt.setLong(j + 1, resultLongMap.get(allResultNames.get(j)));
-											}
-											pstmt.addBatch();
-								
-											resultIntegerMap.clear();
-											resultLongMap.clear();
-											resultIntegerMap = null;
-											resultLongMap = null;
-											flowsDataStructureForDb.removeFlow(fiveTuple);
-										}
-								
-										pstmt.executeBatch();
-										con.commit();
-										pstmt.clearBatch();
-										pstmt.close();
-										long afterInsertTime = System.currentTimeMillis();
-										Statisics.addStatToDbInserts(afterInsertTime-beforeInsertTime);
-										System.out.println("DbInsertsTime= " + (afterInsertTime-beforeInsertTime));
-										
-										listOfFlowsThatShouldAgeFiveTuples.clear();
-										allResultNames.clear();
-										allIntResults.clear();
-										allLongResults.clear();
-										listOfFlowsThatShouldAgeFiveTuples = null;
-										allResultNames = null;
-										allIntResults = null;
-										allLongResults = null;
-									}
-									
-									System.gc();
-								}
-							}
-						}
-			
-						System.out.println("Total Time = " + (System.currentTimeMillis() - startTime) / 1000 + " Seconds");
-						Statisics.exportToCsvFile("c:\\CaptureAnalyzerStats_numPart_" + Statisics.getNumOfDbPartitions() + "_numMaps_" + Statisics.getNumOfMaps() + "_BufSize_" + Statisics.getSizeOfBuffer() + "__Total_" + ((System.currentTimeMillis() - startTime) / 1000) +".csv");
-			
-					} catch (Exception e)
-					{
-						System.out.println("Exception Caught");
-						e.printStackTrace();
+						checkForAgedFlowsAndInsertToDb(agingTime, packetAnalyzers, nextblock, con);
+						
+						System.out.println("Percentage Done: " + frd.getBytesRead() / (float) frd.getCapFileSizeInBytes());
 					}
 				}
 			}
+
+			System.out.println("Total Time = " + (System.currentTimeMillis() - startTime) / 1000 + " Seconds");
+
+		} catch (Exception e)
+		{
+			System.out.println("Exception Caught");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @param agingTime
+	 * @param packetAnalyzers
+	 * @param nextblock
+	 * @param con
+	 * @throws SQLException
+	 */
+	protected static void checkForAgedFlowsAndInsertToDb(final long agingTime, List<IPacketAnalyzer> packetAnalyzers, CaptureFileBlock nextblock, Connection con)
+			throws SQLException
+	{
+		long currentPacketTime = nextblock.getMyPktHdr().getTime();
+		FlowsDataStructure flowsDataStructure = FlowsDataStructure.getInstance();
+		
+		List<FiveTuple> listOfFlowsThatShouldAgeFiveTuples = flowsDataStructure.getAllFlowsThatShouldAgeFiveTuples(currentPacketTime, agingTime);
+
+		for (FiveTuple fiveTuple : listOfFlowsThatShouldAgeFiveTuples)
+		{
+			for (IPacketAnalyzer packetAnalyzer : packetAnalyzers)
+			{
+				packetAnalyzer.finalizeFlow(fiveTuple);
+			}
+		}
+
+		if (listOfFlowsThatShouldAgeFiveTuples.size() > 0)
+		{
+			insertAgedFlowsToDb(listOfFlowsThatShouldAgeFiveTuples, con);
+		}
+	}
+
+	/**
+	 * @param listOfFlowsThatShouldAgeFiveTuples
+	 * @param con
+	 * @param allResultNames
+	 * @throws SQLException
+	 */
+	@SuppressWarnings("serial")
+	protected static void insertAgedFlowsToDb(List<FiveTuple> listOfFlowsThatShouldAgeFiveTuples, Connection con) throws SQLException
+	{
+		FlowsDataStructureForDB flowsDataStructureForDb = FlowsDataStructureForDB.getInstance();
+		List<String> allResultNames = new ArrayList<String>() {};
+		PreparedStatement pstmt;
+		
+		String resultNames = "";
+		String resultValues = "";
+
+		Map<String, Integer> resultIntegerMap = flowsDataStructureForDb.getFlowInfoForDbStruct(listOfFlowsThatShouldAgeFiveTuples.get(0)).getIntegerMap();
+		Map<String, Long> resultLongMap = flowsDataStructureForDb.getFlowInfoForDbStruct(listOfFlowsThatShouldAgeFiveTuples.get(0)).getLongMap();
+		Set<String> allIntResults = resultIntegerMap.keySet();
+		Set<String> allLongResults = resultLongMap.keySet();
+
+		for (String intKey : allIntResults)
+			allResultNames.add(intKey);
+		for (String longKey : allLongResults)
+			allResultNames.add(longKey);
+
+		for (String resultName : allResultNames)
+		{
+			resultNames += resultName + ",";
+			resultValues += "?,";
+		}
+		resultNames = resultNames.substring(0, resultNames.length() - 1);
+		resultValues = resultValues.substring(0, resultValues.length() - 1);
+
+		pstmt = con.prepareStatement("INSERT INTO all_flows(" + resultNames + ") VALUES (" + resultValues + ")");
+		con.setAutoCommit(false);
+		con.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+
+		for (FiveTuple fiveTuple : listOfFlowsThatShouldAgeFiveTuples)
+		{
+			resultIntegerMap = flowsDataStructureForDb.getFlowInfoForDbStruct(fiveTuple).getIntegerMap();
+			resultLongMap = flowsDataStructureForDb.getFlowInfoForDbStruct(fiveTuple).getLongMap();
+
+			for (int j = 0; j < allResultNames.size(); j++)
+			{
+				if (resultIntegerMap.containsKey(allResultNames.get(j)))
+					pstmt.setInt(j + 1, resultIntegerMap.get(allResultNames.get(j)));
+				else if (resultLongMap.containsKey(allResultNames.get(j)))
+					pstmt.setLong(j + 1, resultLongMap.get(allResultNames.get(j)));
+			}
+			pstmt.addBatch();
+
+			resultIntegerMap.clear();
+			resultLongMap.clear();
+			resultIntegerMap = null;
+			resultLongMap = null;
+			flowsDataStructureForDb.removeFlow(fiveTuple);
+		}
+
+		pstmt.executeBatch();
+		con.commit();
+		pstmt.clearBatch();
+		pstmt.close();
+
+		listOfFlowsThatShouldAgeFiveTuples.clear();
+		allResultNames.clear();
+		allIntResults.clear();
+		allLongResults.clear();
+		listOfFlowsThatShouldAgeFiveTuples = null;
+		allResultNames = null;
+		allIntResults = null;
+		allLongResults = null;
+		
+		System.gc();
+	}
+
+	/**
+	 * @param con
+	 */
+	protected static void createDbTable(Connection con, boolean dropTableIfExist)
+	{
+		try
+		{
+			if(dropTableIfExist)
+				con.createStatement().execute("DROP TABLE IF EXISTS `capture_analyzer`.`all_flows`");
+
+			con.createStatement().execute(
+					"CREATE TABLE  `capture_analyzer`.`all_flows` " + "(`flow_id` int(10) unsigned NOT NULL DEFAULT '0'," + "`source_ip` bigint(20) unsigned NOT NULL DEFAULT '0',"
+							+ "`source_port` int(10) unsigned NOT NULL DEFAULT '0'," + "`destination_ip` bigint(20) unsigned NOT NULL DEFAULT '0',"
+							+ "`destination_port` int(10) unsigned NOT NULL DEFAULT '0'," + "`flow_type` int(10) unsigned NOT NULL DEFAULT '0',"
+							+ "`start_time` bigint(20) unsigned NOT NULL DEFAULT '0'," + "`duration` bigint(20) unsigned NOT NULL DEFAULT '0',"
+							+ "`number_of_packets` bigint(20) unsigned NOT NULL DEFAULT '0'," + "`size` int(10) unsigned NOT NULL DEFAULT '0',"
+							+ "`min_packet_size` int(10) unsigned NOT NULL DEFAULT '0'," + "`average_packet_size` int(10) unsigned NOT NULL DEFAULT '0',"
+							+ "`max_packet_size` int(10) unsigned NOT NULL DEFAULT '0'," + "`min_ipg` bigint(20) unsigned NOT NULL DEFAULT '0',"
+							+ "`average_ipg` bigint(20) unsigned NOT NULL DEFAULT '0'," + "`max_ipg` bigint(20) unsigned NOT NULL DEFAULT '0') "
+							+ "ENGINE=InnoDB DEFAULT CHARSET=latin1 " + "PARTITION BY HASH(flow_id) " + "PARTITIONS " + GlobalConfig.Database.getNumberOfPartitions()
+							+ "; ");
+		} catch (SQLException e1)
+		{
+			e1.printStackTrace();
 		}
 	}
 
@@ -262,9 +227,9 @@ public class AnalyzeCaptureFile
 	 */
 	private static void setMySQLConnectInfo()
 	{
-		driverName = "com.mysql.jdbc.Driver";
-		connURL = "jdbc:mysql://localhost/capture_analyzer";
-		username = "root";
-		password = "oklydokly";
+		driverName = GlobalConfig.Database.getDriverName();
+		connURL = GlobalConfig.Database.getConnURL();
+		username = GlobalConfig.Database.getUsername();
+		password = GlobalConfig.Database.getPassword();
 	}
 }
