3beb35f5a6ae9a4f40db526e85b13e81b2ba3094
llasry@cisco.com <llasry@cisco.com@1f3fe170-bcf4-748c-f3f9-3c66050806bb>
2011-02-26 09:17:37 +0000

    uupdated to use nomber of maps from GlobalConf

--- trunk/CaptureAnalyzer/src/capanalyzer/netutils/FlowsDataStructureForDB.java
+++ trunk/CaptureAnalyzer/src/capanalyzer/netutils/FlowsDataStructureForDB.java
@@ -1,18 +1,27 @@
 package capanalyzer.netutils;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import capanalyzer.GlobalConfig;
 import capanalyzer.netutils.build.FiveTuple;
 
 public class FlowsDataStructureForDB
 {
 	private static FlowsDataStructureForDB instance = null;
-	private Map<String, FlowInfoStructForDB> flowsForDbMap = null;
+	private List<Map<String, FlowInfoStructForDB>> listOfTupleMaps = null;
+	
+	private int numberOfMapsToUse = GlobalConfig.CaptureFileReadParams.getNumberOfDbMaps();
 	
 	private FlowsDataStructureForDB()
-	{
-		flowsForDbMap = new HashMap<String, FlowInfoStructForDB>();		
+	{		
+		listOfTupleMaps = new ArrayList<Map<String, FlowInfoStructForDB>>();
+		for (int i = 0; i < numberOfMapsToUse; i++)
+		{
+			listOfTupleMaps.add(new HashMap<String, FlowInfoStructForDB>());		
+		}
 	}
 
 	public static FlowsDataStructureForDB getInstance()
@@ -26,26 +35,34 @@ public class FlowsDataStructureForDB
 	
 	public void addNewFlow(FiveTuple theFlowTuple)
 	{
-		flowsForDbMap.put(theFlowTuple.getKey(), new FlowInfoStructForDB());
+		listOfTupleMaps.get(Math.abs(theFlowTuple.hashCode())%numberOfMapsToUse).put(theFlowTuple.getKey(), new FlowInfoStructForDB());
 	}
 	
 	public boolean ifFlowExist(FiveTuple theFlowTuple)
 	{
-		return flowsForDbMap.containsKey(theFlowTuple.getKey());
+		return listOfTupleMaps.get(Math.abs(theFlowTuple.hashCode())%numberOfMapsToUse).containsKey(theFlowTuple.getKey());
 	}
 	
 	public FlowInfoStructForDB getFlowInfoForDbStruct(FiveTuple theFlowTuple)
 	{
-		return flowsForDbMap.get(theFlowTuple.getKey());
+		return listOfTupleMaps.get(Math.abs(theFlowTuple.hashCode())%numberOfMapsToUse).get(theFlowTuple.getKey());
 	}
 	
 	public void removeFlow(FiveTuple theFlowTuple)
 	{
-		flowsForDbMap.remove(theFlowTuple.getKey());
+		listOfTupleMaps.get(Math.abs(theFlowTuple.hashCode())%numberOfMapsToUse).remove(theFlowTuple.getKey());
 	}
 	
-	public int getNumberOfFlows()
+	public long[] getNumberOfFlows()
 	{
-		return flowsForDbMap.size();
+		long[] numOfTuplesInMaps = new long[listOfTupleMaps.size()];
+		
+		for (int i = 0; i < listOfTupleMaps.size(); i++)
+		{
+			numOfTuplesInMaps[i] = listOfTupleMaps.get(i).size();
+			
+		}
+		
+		return numOfTuplesInMaps;
 	}
 }
