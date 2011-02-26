fed988465b1404dfdf275bf092014e953c65e5a7
llasry <llasry@cisco.com>
2011-02-26 09:16:45 +0000

    updated for database parameters

--- CaptureAnalyzer/src/capanalyzer/model/Model.java
+++ CaptureAnalyzer/src/capanalyzer/model/Model.java
@@ -3,38 +3,46 @@ package capanalyzer.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Model extends ModelObject {
+public class Model extends ModelObject
+{
 	private static Model model;
 	private List<Database> databases = new ArrayList<Database>();
 
-	public Model() {
+	public Model()
+	{
 		Database database = new Database();
-		database.setHostname("My Mail Database");
-		database.setPort(23);
+		database.setHostname("Database");
+		database.setSchema("Test Schema");
 		database.setUsername("test");
 		addDatabase(database);
 	}
-	
-	public void addDatabase(final Database database) {
+
+	public void addDatabase(final Database database)
+	{
 		databases.add(database);
 		firePropertyChange("databases", null, null);
 	}
 
-	public void removeDatabase(Database child) {
+	public void removeDatabase(Database child)
+	{
 		databases.remove(child);
 		firePropertyChange("databases", null, null);
 	}
 
-	public List<Database> getDatabases() {
+	public List<Database> getDatabases()
+	{
 		return databases;
 	}
-	
-	public Object getDefaultSelection() {
+
+	public Object getDefaultSelection()
+	{
 		return databases.size() > 0 ? databases.get(0).getDefaultSelection() : null;
 	}
 
-	public static Model getInstance() {
-		if (model == null) {
+	public static Model getInstance()
+	{
+		if (model == null)
+		{
 			model = new Model();
 		}
 		return model;
