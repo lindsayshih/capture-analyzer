e6c3b33a16e00730f5557777becd194667df6690
llasry@cisco.com <llasry@cisco.com@1f3fe170-bcf4-748c-f3f9-3c66050806bb>
2011-02-26 09:16:03 +0000

    updated for database parameters

--- trunk/CaptureAnalyzer/src/capanalyzer/CreateDatabaseWizard.java
+++ trunk/CaptureAnalyzer/src/capanalyzer/CreateDatabaseWizard.java
@@ -5,23 +5,28 @@ import org.eclipse.jface.wizard.Wizard;
 import capanalyzer.model.Model;
 import capanalyzer.model.Database;
 
-
-public class CreateDatabaseWizard extends Wizard {
+public class CreateDatabaseWizard extends Wizard
+{
 	private final Database database = new Database();
-	public CreateDatabaseWizard() {
+
+	public CreateDatabaseWizard()
+	{
 		// defaults
 		database.setHostname("localhost");
-		database.setPort(23);
 	}
-	public void addPages() {
+
+	public void addPages()
+	{
 		addPage(new CreateDatabasePage(database));
 	}
 
-	public String getWindowTitle() {
-		return "Create Mail Database";
+	public String getWindowTitle()
+	{
+		return "Create Database";
 	}
 
-	public boolean performFinish() {
+	public boolean performFinish()
+	{
 		Model.getInstance().addDatabase(database);
 		return true;
 	}
